4ae2cbbf13ba0583a07b0b490d64fb01462feed3
DajmondFM <franekmalikowski@gmail.com>
2022-04-11 21:01:48 +0200

    yes

--- main.py
+++ main.py
@@ -8,40 +8,52 @@ os.chdir(os.path.dirname(__file__))
 with open('data.json', 'r+') as f:
     data = json.load(f)
 
-# def main():
-#     print("Welcome to Password Manager")
-#     print("1. Add a new password")
-#     print("2. View all passwords")
-#     print("3. Delete an password")
-#     print("4. Exit")
-#     choice = input("Enter your choice: ")
-#     if choice == "1":
-#         add_password()
-#     elif choice == "2":
-#         view_passwords()
-#     elif choice == "3":
-#         delete_password()
-#     elif choice == "4":
-#         exit()
-#     else:
-#         print("Invalid choice")
-#         main()
+def main():
+    print("Welcome to Password Manager by Dajmond")
+    print("1. Add a new password")
+    print("2. View all passwords")
+    print("3. Delete an password")
+    print("4. Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        add_password()
+    elif choice == "2":
+        view_passwords()
+    elif choice == "3":
+        delete_password()
+    elif choice == "4":
+        exit()
+    else:
+        print("Invalid choice, enter again")
+        main()
 
-# main()
+def add_password():
+    site = input("Enter the site url: ")
+    login = input("Enter the login: ")
+    password = input("Enter the password: ")
+    data[site] = {
+        "login": login,
+        "password": password
+    }
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
+    print("Password added")
+    main()
 
-print("What would you like to do?")
-print("1. Add a password")
-print("2. View a passwords")
-print("3. Delete a password")
+def view_passwords():
+    for site in data:
+        print(site)
+    main()
 
+def delete_password():
+    site = input("Enter the site: ")
+    if site in data:
+        del data[site]
+        with open('data.json', 'w') as f:
+            json.dump(data, f)
+        print("Password deleted")
+    else:
+        print("Password not found")
+    main()
 
-
-a = int(input("Enter a number: "))
-if a == 1:
-    print("You have chosen to add a password")
-    print("Enter the name of the password")
-    name = input("Name: ")
-    print("Enter the password")
-    password = input("Password: ")
-    data["Nazwa1"]["Haslo"] = password
-    json.dump(data, open('data.json', 'w'))
+main()
