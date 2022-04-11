06250ac852bc7b4c8c5afb9cd0f03146900b053f
DajmondFM <franekmalikowski@gmail.com>
2022-04-11 21:39:10 +0200

    0.1 version

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import json
 os.chdir(os.path.dirname(__file__))
 
 # Open json file
+# Create a data.json
+# If file doesn't exist, create it
+if not os.path.exists('data.json'):
+    with open('data.json', 'w') as f:
+        data = {}
+        json.dump(data, f)
+
 with open('data.json', 'r+') as f:
     data = json.load(f)
 
@@ -12,16 +19,20 @@ def main():
     print("Welcome to Password Manager by Dajmond")
     print("1. Add a new password")
     print("2. View all passwords")
-    print("3. Delete an password")
-    print("4. Exit")
+    print("3. Show password")
+    print("4. Delete an password")
+    print("5. Exit")
     choice = input("Enter your choice: ")
+    print("")
     if choice == "1":
         add_password()
     elif choice == "2":
         view_passwords()
     elif choice == "3":
-        delete_password()
+        show_password()
     elif choice == "4":
+        delete_password()
+    elif choice == "5":
         exit()
     else:
         print("Invalid choice, enter again")
@@ -38,11 +49,13 @@ def add_password():
     with open('data.json', 'w') as f:
         json.dump(data, f)
     print("Password added")
+    print("")
     main()
 
 def view_passwords():
     for site in data:
         print(site)
+    print("")
     main()
 
 def delete_password():
@@ -51,7 +64,17 @@ def delete_password():
         del data[site]
         with open('data.json', 'w') as f:
             json.dump(data, f)
-        print("Password deleted")
+        print("Password deleted \n")
+    else:
+        print("Password not found")
+    main()
+
+def show_password():
+    site = input("Enter the site: ")
+    if site in data:
+        print("Login: " + data[site]["login"])
+        print("Password: " + data[site]["password"])
+        print("")
     else:
         print("Password not found")
     main()
